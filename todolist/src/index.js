3a174aa505e85b011bc4bca7d8660af457646475
surbir101 <ssandhu12@horizon.csueastbay.edu>
2021-10-06 16:29:32 -0700

    breaking app into components

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-ReactDOM.render(<App subject="Surbir" />, document.getElementById('root'));
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
