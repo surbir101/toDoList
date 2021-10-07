f6a601345199fd795bbda7362ac0ff27177df78c
surbir101 <ssandhu12@horizon.csueastbay.edu>
2021-10-07 11:13:38 -0700

    adding functionality for delete, and toggle tasks

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -5,21 +5,42 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
-  const [tasks, setTasks] = useState(props.tasks);
 
-  function addTask(name) {
-    const newTask = {id: "todo-" + nanoid(), name: name, completed: false };
-    setTasks([...tasks,newTask]);
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if(id === task.id) {
+        return {...task,completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   }
 
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks); 
+  }
+
+  const [tasks, setTasks] = useState(props.tasks);
   const taskList = tasks.map(task =>
   <Todo
   id={task.id}
   name={task.name}
   completed={task.completed}
   key={task.id}
+  toggleTaskCompleted={toggleTaskCompleted}
+  deleteTask={deleteTask}
    />
-);
+ );
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+ function addTask(name) {
+   const newTask = {id: "todo-" + nanoid(), name: name, completed: false };
+   setTasks([...tasks,newTask]);
+ }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -29,7 +50,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
